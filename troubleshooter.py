5f900764278c3c03721a307af4efe9ebc361d26c
rukiyearican <100317918+rukiyearican@users.noreply.github.com>
2022-10-02 07:40:19 +0300

    Add files via upload

---
+++ troubleshooter.py
@@ -0,0 +1,9 @@
+import time
+time.sleep(3) # Sleep for 3 seconds
+
+def Windows_Diagnostics():
+    print("looking for problems")
+    time.sleep(15)
+    print("unable to fix the issue")
+
+Windows_Diagnostics()
